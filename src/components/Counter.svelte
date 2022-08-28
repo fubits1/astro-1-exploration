77ef6b850df19c4685360585ca97731aff588bbe
fubits <ilja_s@protonmail.com>
2022-08-28 16:21:15 +0200

    svelte component; Data.map()

---
+++ src/components/Counter.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let count = 0;
+</script>
+
+<div>
+  <button on:click={() => count--}>-</button>
+  <code style="font-size: {22 + count}px">{count}</code>
+  <button on:click={() => count++}>+</button>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  button {
+    margin: 0 1rem;
+    width: 40px;
+  }
+
+  code {
+    font-weight: 700;
+  }
+</style>
